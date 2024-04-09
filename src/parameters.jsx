2e6994a60fa1ac7e9621b90ba306d1a92498a538
ioddgamers <toluloperbrts@gmail.com>
2024-04-09 12:40:52 +0100

    added placeholder for all input elements

--- src/parameters.jsx
+++ src/parameters.jsx
@@ -4,15 +4,15 @@ export const Parameters = () => {
             <form action="">
                 <div className="day-input">
                 <label htmlFor="day"> Day</label><br />
-                <input type="text" id="day" name="day"/>
+                <input type="text" id="day" name="day" placeholder="DD"/>
                 </div>
                 <div className="month-input">
                 <label htmlFor="month"> Month</label><br />
-                <input type="text" id="month" name="month"/>
+                <input type="text" id="month" name="month" placeholder="MM"/>
                 </div>
                 <div className="year-input">
                 <label htmlFor="year"> Year</label><br />
-                <input type="text" id="year" name="year"/>
+                <input type="text" id="year" name="year" placeholder="YYYY"/>
                 </div>
             </form>
         </div>
