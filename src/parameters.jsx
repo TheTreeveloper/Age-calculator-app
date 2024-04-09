ca0bc38c7f8cc8f1765924560eced900b882e60f
ioddgamers <toluloperbrts@gmail.com>
2024-04-09 11:48:06 +0100

    Created parameters for day, month and year, exported parameters

---
+++ src/parameters.jsx
@@ -0,0 +1,20 @@
+export const Parameters = () => {
+    return (
+        <div>
+            <form action="">
+                <div className="day-input">
+                <label htmlFor="day"> Day</label><br />
+                <input type="text" id="day" name="day"/>
+                </div>
+                <div className="month-input">
+                <label htmlFor="month"> Month</label><br />
+                <input type="text" id="month" name="month"/>
+                </div>
+                <div className="year-input">
+                <label htmlFor="year"> Year</label><br />
+                <input type="text" id="year" name="year"/>
+                </div>
+            </form>
+        </div>
+    )
+}   
