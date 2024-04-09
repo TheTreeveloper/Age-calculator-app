d9a4cf4c326522276950aeeae97d5a537dba69a3
ioddgamers <toluloperbrts@gmail.com>
2024-04-09 12:39:40 +0100

    imported linebreaker

--- src/container.jsx
+++ src/container.jsx
@@ -1,9 +1,11 @@
 import { Parameters } from "./parameters";
+import { LineBreak } from "./parameters";
 
 export const Container = () => {
     return (
         <div className="container">
             <Parameters/>
+            <LineBreak/>
         </div>
     );
 }
