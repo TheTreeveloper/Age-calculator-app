39397ad51ebb891bc55fcddb13366f91afe451e5
ioddgamers <toluloperbrts@gmail.com>
2024-04-09 14:14:50 +0100

    created the calculator component

---
+++ src/calculator.jsx
@@ -0,0 +1,9 @@
+export const Calculator = () => {
+    return (
+        <div className="calculator">
+            <p><span>--</span>years</p>
+            <p><span>--</span>months</p>
+            <p><span>--</span>days</p>
+        </div>
+    );
+}
