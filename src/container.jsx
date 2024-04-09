ac88b04426e0aa74312afb0547085d621132828b
ioddgamers <toluloperbrts@gmail.com>
2024-04-09 11:48:19 +0100

    imported parameters

---
+++ src/container.jsx
@@ -0,0 +1,9 @@
+import { Parameters } from "./parameters";
+
+export const Container = () => {
+    return (
+        <div className="container">
+            <Parameters/>
+        </div>
+    );
+}
